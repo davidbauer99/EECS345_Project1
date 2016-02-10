fa6908a976b5cd26b6ff21ddd38a2953ab5e2dc8
David Bauer <dmb172@case.edu>
2016-02-10 14:18:54 -0500

    Added lex.scm and simpleParser.scm, reorganized project_1.rkt based on function type.
    Signed-off-by: David Bauer <dmb172@case.edu>

--- project_1.rkt
+++ project_1.rkt
@@ -2,6 +2,11 @@
 ;David Bauer dmb172
 ;Ryan Nowacoski rmn36
 
+;Load the parser
+(load "simpleParser.scm")
+
+
+;;;;;;;;;;;;;;;;; M_value functions ;;;;;;;;;;;;;;;;;;
 (define M_value
   (lambda (expression state)
     (cond
@@ -45,11 +50,64 @@
 
 (define operand2 caddr)
 
+;MStateReturn will take a return statement and return the statement right of "return"
+(define M_value-return
+  (lambda (expression state)
+    (M_value (cadr expression) state)))
+
+;;;;;;;;;;;;;;;;;;;;;;; M_state section ;;;;;;;;;;;;;;;;;;
+
 (define M_state-while
   (lambda (condition statement state)
     (if (M_boolean condition state)
         (M_state-while condition statement (M_state statement state))
         state)))
+
+(define M_state-if
+  (lambda (statement state)
+    (if (M_value (GetCondition statement) state)
+        (M_state-if (GetThenStatement statement) state)
+        (M_state-if (GetOptElse statement) state))))
+
+;Helper for if statement to get the first condition
+(define GetCondition cadr)
+
+;Helper for if statment to get the "then" statement
+(define GetThenStatement caddr)
+
+;Helper for if statement to get the optional else statement
+(define GetOptElse
+  (lambda (l)
+    (if (null? (cdddr l))
+        '()
+        (cadddr l))))
+
+;M_declare_statement will take a list starting with 'var followed by an atom with an optional value
+(define M_state-declare
+  (lambda (statement state)
+    (cond
+      ((not (eq? 'var (car statement))) (error 'illegal "Declaration statment does not start with 'var'"))
+      ((null? (declare-value-list statement)) (declare_var (declare-var-name statement) state))
+      (else (update_state (declare-var-name statement) (M_value (declare-val statement) state) (declare_var (declare-var-name statement) state))))))
+
+(define declare-value-list cddr)
+
+(define declare-val caddr)
+
+(define declare-var-name cadr)
+
+; M_assign takes a statement and state and updates the state with the desired variable assignment.
+(define M_state-assign
+  (lambda (statement state)
+    (cond
+      ((not (eq? '= (car statement))) (error 'illegal "Assignment statement does not start with '='"))
+      (else (update_state (assign-var statement) (M_value (assign-expression statement) state) state)))))
+
+(define assign-var cadr)
+
+(define assign-expression caddr)
+
+;;;;;;;;;;;;;;; State manipulation and management ;;;;;;;;;;;;;;;;;;;;;;;
 
 (define lookup
   (lambda (name state)
@@ -76,12 +134,24 @@
   (lambda (expression)
     (not (or (pair? expression) (null? expression)))))
 
+; declare_var adds a new variable to the state with 'undefined as its initial value
 (define declare_var
   (lambda (name state)
     (cond
       ((null? state) 'undefined)
+      ((contains? name (car state)) (error 'error "Duplicate delcaration of variable."))
       (else (cons (cons name (variables state)) (list (cons 'undefined (state_values state))))))))
 
+(define contains?
+  (lambda (v l)
+    (cond
+      ((null? l) #f)
+      ((eq? v (car l)) #t)
+      (else (contains? v (cdr l))))))
+        
+    
+
+; update_state takes a name and a value and updates that name with the value if it exists in the state
 (define update_state
   (lambda (name value state)
     (cond
@@ -91,53 +161,3 @@
       (else ((lambda (newState)
                (cons (cons (first_variable state) (variables newState)) (list (cons (first_value state) (state_values newState)))))
              (update_state name value (cons (remaining_variables state) (list (remaining_values state)))))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;If-statement Section;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define M_state-if
-  (lambda (statement state)
-    (if (M_value (GetCondition statement) state)
-        (M_state-if (GetThenStatement statement) state)
-        (M_state-if (GetOptElse statement) state))))
-
-;Helper for if statement to get the first condition
-(define GetCondition cadr)
-
-;Helper for if statment to get the "then" statement
-(define GetThenStatement caddr)
-
-;Helper for if statement to get the optional else statement
-(define GetOptElse
-  (lambda (l)
-    (if (null? (cdddr l))
-        '()
-        (cadddr l))))
-
-;MStateReturn will take a return statement and return the statement right of "return"
-(define M_value-return
-  (lambda (expression state)
-    (M_value (cadr expression) state)))
-
-;M_declare_statement will take a list starting with 'var followed by an atom with an optional value
-(define M_declare
-  (lambda (statement state)
-    (cond
-      ((not (eq? 'var (car statement))) (error 'illegal "Declaration statment does not start with 'var'"))
-      ((null? (declare-value-list statement)) (declare_var (declare-var-name statement) state))
-      (else (update_state (declare-var-name statement) (M_value (declare-val statement) state) (declare_var (declare-var-name statement) state))))))
-
-(define declare-value-list cddr)
-
-(define declare-val caddr)
-
-(define declare-var-name cadr)
-
-; M_assign takes a statement and state and updates the state with the desired variable assignment.
-(define M_assign
-  (lambda (statement state)
-    (cond
-      ((not (eq? '= (car statement))) (error 'illegal "Assignment statement does not start with '='"))
-      (else (update_state (assign-var statement) (M_value (assign-expression statement) state) state)))))
-
-(define assign-var cadr)
-
-(define assign-expression caddr)
