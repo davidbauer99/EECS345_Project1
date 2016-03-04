99ffb6e6ee3bb560312eb5413c055bcbb61389db
David Bauer <dmb172@case.edu>
2016-03-03 19:28:35 -0500

    Implemented continuations for if and return.
    Signed-off-by: David Bauer <dmb172@case.edu>

--- project_2.rkt
+++ project_2.rkt
@@ -16,13 +16,15 @@
       ((atom? state) state)
       (else (M_state (firstStatement statements) state
                      (lambda (s) (interpret_parsed (remaining statements) s))
-                     baseBreak baseContinue baseThrow)))))
+                     baseBreak baseContinue baseThrow baseReturn)))))
 
 (define baseBreak (lambda (s) (error 'error "Break outside of block")))
 
 (define baseContinue (lambda (s) (error 'error "Continue outside of block")))
 
 (define baseThrow (lambda (v s) (error 'error "Throw outside of try block")))
+
+(define baseReturn (lambda (v s) v))
 
 (define remaining cdr)
 
@@ -74,8 +76,8 @@
 
 ;M_value-return will take a return statement and return the statement right of "return"
 (define M_value-return
-  (lambda (expression state)
-    (convert_value (M_value (evaluatable expression) state))))
+  (lambda (expression state return)
+    (return (convert_value (M_value (evaluatable expression) state)) state)))
 
 (define evaluatable cadr)
 
@@ -91,19 +93,19 @@
 ;;;;;;;;;;;;;;;;;;;;;;; M_state section ;;;;;;;;;;;;;;;;;;
 ; lamnda (statement state return break continue throw)
 (define M_state
-  (lambda (statement state next break continue throw)
+  (lambda (statement state next break continue throw return)
     (cond
       ((null? (operation statement)) (error 'error "Empty statement."))
       ((eq? 'var (operation statement)) (M_state-declare statement state next))
-      ((eq? '= (operation statement)) (M_state-assign statement state next break continue throw))
-      ((eq? 'try (operation statement)) (M_state-try statement state next break continue throw))
-      ((eq? 'begin (operation statement)) (M_state-begin statement state next break continue throw))
+      ((eq? '= (operation statement)) (M_state-assign statement state next break continue throw return))
+      ((eq? 'try (operation statement)) (M_state-try statement state next break continue throw return))
+      ((eq? 'begin (operation statement)) (M_state-begin statement state next break continue throw return))
       ((eq? 'throw (operation statement)) (M_state-throw statement state throw))
       ((eq? 'break (operation statement)) (M_state-break statement state break))
       ((eq? 'continue (operation statement)) (M_state-continue statement state continue))
-      ((eq? 'return (operation statement)) (M_value-return statement state))
-      ((eq? 'if (operation statement)) (M_state-if statement state))
-      ((eq? 'while (operation statement)) (M_state-while statement state next break continue throw))
+      ((eq? 'return (operation statement)) (M_value-return statement state return))
+      ((eq? 'if (operation statement)) (M_state-if statement state next break continue throw return))
+      ((eq? 'while (operation statement)) (M_state-while statement state next break continue throw return))
       (else (error 'error "Unrecognized statement type.")))))
 
 (define M_state-add-frame
@@ -115,22 +117,23 @@
     (next state)))
 
 (define M_state-begin
-  (lambda (statement state next break continue throw)
+  (lambda (statement state next break continue throw return)
     (cond
       ((not (eq? (car statement) 'begin)) (error 'error "Begin block without 'begin' at the beginning."))
       (else (M_state-add-frame state (lambda (st) (M_state-block (cdr statement) st
                                                                  (lambda (s) (M_state-pop-frame s (lambda (s2) (next s2))))
                                                                  (lambda (s) (M_state-pop-frame s (lambda (s2) (break s2))))
                                                                  (lambda (s) (M_state-pop-frame s (lambda (s2) (continue s2))))
-                                                                 (lambda (s) (M_state-pop-frame s (lambda (s2) (throw s2)))))))))))
+                                                                 (lambda (v s) (M_state-pop-frame s (lambda (s2) (throw v s2))))
+                                                                 (lambda (v s) (M_state-pop-frame s (lambda (s2) (return v s2)))))))))))
 
 (define M_state-block
-  (lambda (statements state next break continue throw)
+  (lambda (statements state next break continue throw return)
     (cond
       ((null? statements) (next state))
       (else (M_state (firstStatement statements) state
-                     (lambda (s) (M_state-block (remaining statements) s next break continue throw))
-                     break continue throw)))))
+                     (lambda (s) (M_state-block (remaining statements) s next break continue throw return))
+                     break continue throw return)))))
 
 (define M_state-throw
   (lambda (statement state throw)
@@ -147,19 +150,21 @@
 (define throwVar cadr)
 
 (define M_state-try
-  (lambda (statement state next break continue throw)
+  (lambda (statement state next break continue throw return)
     (if (null? (finallyBlock statement))
         (M_state-add-frame state (lambda (st) (M_state-try-catch (tryBlock statement) (catchBlock statement) st
                                                                  (lambda (s) (M_state-pop-frame s (lambda (s2) (next s2))))
                                                                  (lambda (s) (M_state-pop-frame s (lambda (s2) (break s2))))
                                                                  (lambda (s) (M_state-pop-frame s (lambda (s2) (continue s2))))
-                                                                 (lambda (v s) (M_state-pop-frame s (lambda (s2) (throw v s2)))))))
+                                                                 (lambda (v s) (M_state-pop-frame s (lambda (s2) (throw v s2))))
+                                                                 (lambda (v s) (M_state-pop-frame s (lambda (s2) (return v s2)))))))
         (M_state-add-frame state (lambda (st) (M_state-try-catch-finally
                                                (tryBlock statement) (catchBlock statement) (finallyStatements statement) st
                                                (lambda (s) (M_state-pop-frame s (lambda (s2) (next s2))))
                                                (lambda (s) (M_state-pop-frame s (lambda (s2) (break s2))))
                                                (lambda (s) (M_state-pop-frame s (lambda (s2) (continue s2))))
-                                               (lambda (v s) (M_state-pop-frame s (lambda (s2) (throw v s2))))))))))
+                                               (lambda (v s) (M_state-pop-frame s (lambda (s2) (throw v s2))))
+                                               (lambda (v s) (M_state-pop-frame s (lambda (s2) (return v s2))))))))))
 
 (define tryBlock cadr)
 
@@ -172,18 +177,18 @@
 (define finallyBlock cadddr)
 
 (define M_state-try-catch
-  (lambda (try catch state next break continue throw)
+  (lambda (try catch state next break continue throw return)
     (M_state-block try state next break continue
-                   (lambda (v s) (M_state-catch catch v s next break continue throw)))))
+                   (lambda (v s) (M_state-catch catch v s next break continue throw return)) return)))
 
 (define M_state-catch
-  (lambda (catch value state next break continue throw)
+  (lambda (catch value state next break continue throw return)
     (cond
       ((null? value) (error 'error "Null value was thrown."))
       (else (M_state-pop-frame state
                                (lambda (st) (M_state-add-frame st
                                                                (lambda (s) (M_state-declare (buildDeclare (varName catch) value) s
-                                                                                            (lambda (s2) (M_state-block (catchStatements catch) s2 next break continue throw)))))))))))
+                                                                                            (lambda (s2) (M_state-block (catchStatements catch) s2 next break continue throw return)))))))))))
 
 (define catchStatements cadr)
       
@@ -195,47 +200,50 @@
 (define varName caar)
 
 (define M_state-try-catch-finally
-  (lambda (try catch finally state next break continue throw)
+  (lambda (try catch finally state next break continue throw return)
     (M_state-block try state
-                   (lambda (s) (finally-next-continuation finally s next break continue throw)); next
-                   (lambda (s) (finally-break-continuation finally s next break continue throw)) ; break
-                   (lambda (s) (finally-continue-continuation finally s next break continue throw)) ; continue
-                   (lambda (v s) (finally-throw-continuation catch v finally s next break continue throw)))))
+                   (lambda (s) (finally-next-continuation finally s next break continue throw return)); next
+                   (lambda (s) (finally-break-continuation finally s next break continue throw return)) ; break
+                   (lambda (s) (finally-continue-continuation finally s next break continue throw return)) ; continue
+                   (lambda (v s) (finally-throw-continuation catch v finally s next break continue throw return))
+                   (lambda (v s) (finally-return-continuation finally s next break continue throw return)))))
 
 (define M_state-finally
-  (lambda (statements state next break continue throw)
+  (lambda (statements state next break continue throw return)
     (M_state-pop-frame state
                        (lambda (st) (M_state-add-frame st
-                                                       (lambda (s) (M_state-block statements s next break continue throw)))))))
+                                                       (lambda (s) (M_state-block statements s next break continue throw return)))))))
 
 (define finally-next-continuation
-  (lambda (finally state next break continue throw)
-    (M_state-finally finally state (lambda (s) (next s)) break continue throw)))
+  (lambda (finally state next break continue throw return)
+    (M_state-finally finally state (lambda (s) (next s)) break continue throw return)))
                
 (define finally-break-continuation
-  (lambda (finally state next break continue throw)
-    (M_state-finally finally state (lambda (s) (break s)) break continue throw)))
+  (lambda (finally state next break continue throw return)
+    (M_state-finally finally state (lambda (s) (break s)) break continue throw return)))
 
 (define finally-continue-continuation
-  (lambda (finally state next break continue throw)
-    (M_state-finally finally state (lambda (s) (continue s)) break continue throw)))
+  (lambda (finally state next break continue throw return)
+    (M_state-finally finally state (lambda (s) (continue s)) break continue throw return)))
 
 (define finally-throw-continuation
-  (lambda (catch value finally state next break continue throw)
+  (lambda (catch value finally state next break continue throw return)
     (M_state-catch catch value state
-                   (lambda (s) (finally-next-continuation finally s next break continue throw))
-                   (lambda (s) (finally-break-continuation finally s next break continue throw))
-                   (lambda (s) (finally-continue-continuation finally s next break continue throw))
-                   (lambda (v s) (M_state-finally finally s (lambda (s2) (throw v s2)) break continue throw)))))
+                   (lambda (s) (finally-next-continuation finally s next break continue throw return))
+                   (lambda (s) (finally-break-continuation finally s next break continue throw return))
+                   (lambda (s) (finally-continue-continuation finally s next break continue throw return))
+                   (lambda (v s) (M_state-finally finally s (lambda (s2) (throw v s2)) break continue throw return))
+                   (lambda (v s) (finally-return-continuation finally s next break continue throw return)))))
 
 (define M_state-while
-  (lambda (statement state next break continue throw)
+  (lambda (statement state next break continue throw return)
     (if (M_value-boolean (GetCondition statement) state)
         (M_state (GetStatement statement) state
-                 (lambda (s) (M_state-while statement s next break continue throw))
+                 (lambda (s) (M_state-while statement s next break continue throw return))
                  (lambda (s) (next s))
-                 (lambda (s) (M_state-while statement s next break continue throw))
-                 throw)
+                 (lambda (s) (M_state-while statement s next break continue throw return))
+                 throw
+                 return)
         (next state))))
         ;(M_state-while statement (M_state (GetStatement statement) state))
         ;state)))
@@ -243,11 +251,14 @@
 (define GetStatement caddr)
 
 (define M_state-if
-  (lambda (statement state)
+  (lambda (statement state next break continue throw return)
     (cond
-      ((M_value (GetCondition statement) state) (M_state (GetThenStatement statement) state))
-      ((not (null? (GetOptElse statement))) (M_state (GetOptElse statement) state))
-      (else state))))
+      ((M_value (GetCondition statement) state) (M_state (GetThenStatement statement) state next break continue throw return))
+      ((not (null? (GetOptElse statement))) (M_state (GetOptElse statement) state next break continue throw return))
+      (else (next state)))))
+      ;((M_value (GetCondition statement) state) (M_state (GetThenStatement statement) state))
+      ;((not (null? (GetOptElse statement))) (M_state (GetOptElse statement) state))
+      ;(else state))))
 
 ;Helper for if statement to get the first condition
 (define GetCondition cadr)
@@ -282,7 +293,7 @@
 
 ; M_assign takes a statement and state and updates the state with the desired variable assignment.
 (define M_state-assign
-  (lambda (statement state next break continue throw)
+  (lambda (statement state next break continue throw return)
     (cond
       ((not (eq? '= (operation statement))) (error 'illegal "Assignment statement does not start with '='"))
       (else (next (update_state (assign-var statement) (M_value (assign-expression statement) state) state))))))
